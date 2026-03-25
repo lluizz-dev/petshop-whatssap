8a56e1bff07e17b06beba1aed28ad60c27c99468
lluizz-dev <luiz.eduardodss24@gmail.com>
2026-03-25 15:01:11 -0300

    refactor: update the models

--- sistema_mensagem/models.py
+++ sistema_mensagem/models.py
@@ -8,6 +8,15 @@ class Dono(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     telefone = models.CharField(max_length=15)
+    ativo = models.BooleanField(default=True)
+    criado_em = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return self.nome
+    
+class Especie(models.Model):
+    nome = models.CharField(max_length=50)
+    emojis = models.CharField(max_length=5)
 
     def __str__(self):
         return self.nome
@@ -15,7 +24,7 @@ class Dono(models.Model):
 
 class Pet(models.Model):
     nome = models.CharField(max_length=100)
-    especie = models.CharField(max_length=50)
+    especie = models.ForeignKey(Especie, on_delete=models.CASCADE)
     dono = models.ForeignKey(Dono, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -33,9 +42,14 @@ class Vacina(models.Model):
 class Vacinacao(models.Model):
     pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
     vacina = models.ForeignKey(Vacina, on_delete=models.CASCADE)
+    
     data_aplicada = models.DateField()
     data_proxima = models.DateField(blank=True, null=True, editable=False)
-    lembrete_enviado = models.BooleanField(default=False)
+    
+    notificado = models.BooleanField(default=False)
+    data_notificacao = models.DateTimeField(null=True, blank=True)
+    
+    criado_em = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
         if self.data_aplicada and self.vacina:
