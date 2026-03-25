02cb0a4c48725a5278d8b77e1b7dbf5a167df054
lluizz-dev <luiz.eduardodss24@gmail.com>
2026-03-25 16:53:17 -0300

    feat: create dashboard and aply configurations

--- sistema_mensagem/views.py
+++ sistema_mensagem/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.utils import timezone
+from datetime import timedelta
+from .models import Vacinacao
 
 # Create your views here.
+
+def dashboard(request):
+    hoje = timezone.localdate()
+    amanha = hoje + timedelta(days=1)
+
+    vacinas_amanha = Vacinacao.objects.filter(data_proxima=amanha, notificado=False)
+    vacinas_atrasadas = Vacinacao.objects.filter(data_proxima__lt=hoje, notificado=False)
+
+    context = {
+        'amanha': vacinas_amanha,
+        'atrasadas': vacinas_atrasadas
+    }
+
+    return render(request, 'sistema_mensagem/dashboard.html', context)
