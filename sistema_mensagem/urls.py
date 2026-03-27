ebf8b0a3666c625bfe27d1a91600f6507a2873b4
lluizz-dev <luiz.eduardodss24@gmail.com>
2026-03-27 10:24:54 -0300

    feat: create listagem_home and listar

--- sistema_mensagem/urls.py
+++ sistema_mensagem/urls.py
@@ -2,12 +2,22 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # Dashboard
     path('', dashboard, name='dashboard'),
     
+    # Páginas de cadastro
     path('cadastro/', cadastro_home, name='cadastro_home'),
     path('cadastro/dono/', cadastrar_dono, name='cadastrar_dono'),
     path('cadastro/especie/', cadastrar_especie, name='cadastrar_especie'),
     path('cadastro/pet/', cadastrar_pet, name='cadastrar_pet'),
     path('cadastro/vacina/', cadastrar_vacina, name='cadastrar_vacina'),
     path('cadastro/vacinacao/', cadastrar_vacinacao, name='cadastrar_vacinacao'),
+    
+    # Páginas de listagem
+    path('listagem/', listagem_home, name='listagem_home'),
+    path('listagem/donos/', listar_donos, name='listar_donos'),
+    path('listagem/especies/', listar_especies, name='listar_especies'),
+    path('listagem/pets/', listar_pets, name='listar_pets'),
+    path('listagem/vacinas/', listar_vacinas, name='listar_vacinas'),
+    path('listagem/vacinacoes/', listar_vacinacoes, name='listar_vacinacoes'),
 ]
--- sistema_mensagem/views.py
+++ sistema_mensagem/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
 from datetime import timedelta
-from .models import Vacinacao
+from .models import Dono, Especie, Pet, Vacina, Vacinacao
 from .forms import DonoForm, EspecieForm, PetForm, VacinaForm, VacinacaoForm
 
 # Create your views here.
@@ -60,3 +60,53 @@ def cadastrar_vacinacao(request):
         form.save()
         return redirect('cadastro_home')
     return render(request, 'sistema_mensagem/form.html', {'form': form, 'titulo': 'Cadastrar Vacinação'})
+
+def listagem_home(request):
+    return render(request, 'sistema_mensagem/listagem_home.html')
+
+def listar_donos(request):
+    donos = Dono.objects.all()
+
+    return render(request, 'sistema_mensagem/listar.html', {
+        'objetos': donos,
+        'titulo': '👤 Lista de Donos'
+    })
+
+def listar_especies(request):
+    especies = Especie.objects.all()
+
+    return render(request, 'sistema_mensagem/listar.html', {
+        'objetos': especies,
+        'titulo': '🐾 Lista de Espécies'
+    })
+
+def listar_pets(request):
+    pets = Pet.objects.select_related('dono', 'especie')
+
+    return render(request, 'sistema_mensagem/listar.html', {
+        'objetos': pets,
+        'titulo': '🐶 Lista de Pets'
+    })
+
+def listar_vacinas(request):
+    vacinas = Vacina.objects.all()
+    
+    return render(request, 'sistema_mensagem/listar.html', {
+        'objetos': vacinas,
+        'titulo': '💉 Lista de Vacinas'
+    })
+
+def listar_vacinacoes(request):
+    vacinacoes = Vacinacao.objects.all()
+    
+    return render(request, 'sistema_mensagem/listar.html', {
+        'objetos': vacinacoes,
+        'titulo': '📅 Lista de Vacinações'
+    })
+
+def editar_home(request):
+    return render(request, 'sistema_mensagem/editar_home.html')
+
+
+def excluir_home(request):
+    return render(request, 'sistema_mensagem/excluir_home.html')
