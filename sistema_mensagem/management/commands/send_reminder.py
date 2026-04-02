de48de7998f41274031f2cda133f5ae57e3e843e
lluizz-dev <luiz.eduardodss24@gmail.com>
2026-04-02 19:48:49 -0300

    refactor: refactor send_reminder, add enviar_lembretes()

--- sistema_mensagem/management/commands/send_reminder.py
+++ sistema_mensagem/management/commands/send_reminder.py
@@ -16,14 +16,6 @@ class Command(BaseCommand):
         amanha = hoje + timedelta(days=1)
         tres_dias = hoje + timedelta(days=3)
         
-        
-
-        account_sid = os.getenv("TWILIO_SID")
-        auth_token = os.getenv("TWILIO_TOKEN")
-        from_whatsapp = os.getenv("TWILIO_WHATSAPP")
-
-        client = Client(account_sid, auth_token)
-        
         vacinas_1_dia = Vacinacao.objects.filter(
             data_proxima=amanha,
             notificado_1_dia=False,
@@ -39,8 +31,19 @@ class Command(BaseCommand):
         if not vacinas_1_dia.exists() and not vacinas_3_dias.exists():
             print("Nenhum lembrete.")
             return
+        
+        enviar_lembretes(vacinas_1_dia, 1)
+        enviar_lembretes(vacinas_3_dias, 3)
+ 
+def enviar_lembretes(vacinas_para_enviar, dias_para_vencimento):
+    
+    account_sid = os.getenv("TWILIO_SID")
+    auth_token = os.getenv("TWILIO_TOKEN")
+    from_whatsapp = os.getenv("TWILIO_WHATSAPP")
 
-        for v in vacinas_1_dia:
+    client = Client(account_sid, auth_token)
+        
+    for v in vacinas_para_enviar:
             try:
                 mensagem = f"""
 🐾 Olá, {v.pet.dono.nome}! Aqui é do petshop Cantinho Cão e Gato.
@@ -59,40 +62,20 @@ Para manter a saúde dele(a) em dia 🐶🐈, recomendamos agendar a próxima do
                     to=numero
                 )
 
-                v.notificado_1_dia = True
-                v.data_notificacao_1_dia = timezone.now()
+                if dias_para_vencimento == 1:
+                    v.notificado_1_dia = True
+                    v.data_notificacao_1_dia = timezone.now()
+                elif dias_para_vencimento == 3:
+                    v.notificado_3_dias = True
+                    v.data_notificacao_3_dias = timezone.now()
+
                 v.save()
-
-                print(f"Mensagem enviada para {numero} as {v.data_notificacao_1_dia} sobre o pet {v.pet.nome} com vacina vencendo em {v.data_proxima.strftime('%d/%m')}")
-
-            except Exception as e:
-                print(f"Erro ao enviar para {numero} as {timezone.now()}: {e}")
                 
-        for v in vacinas_3_dias:
-            try:
-                mensagem = f"""
-🐾 Olá, {v.pet.dono.nome}! Aqui é do petshop Cantinho Cão e Gato.
+                if dias_para_vencimento == 1:
+                    print(f"Mensagem enviada para {numero} as {v.data_notificacao_1_dia} sobre o pet {v.pet.nome} com vacina vencendo em {v.data_proxima.strftime('%d/%m')}")
 
-A vacina do *{v.pet.nome}* {v.pet.especie.emojis} vence em {v.data_proxima.strftime('%d/%m')} 💉
-
-Para manter a saúde dele(a) em dia 🐶🐈, recomendamos agendar a próxima dose.
-
-> Esse número não responde mensagens, Fale com {os.getenv('NUMERO_SUPORTE')} para marcar!
-"""
-
-                numero = f"whatsapp:+55{v.pet.dono.telefone}"
-                
-                client.messages.create(
-                    body=mensagem,
-                    from_=from_whatsapp,
-                    to=numero
-                )
-
-                v.notificado_3_dias = True
-                v.data_notificacao_3_dias = timezone.now()
-                v.save()
-
-                print(f"Mensagem enviada para {numero} as {v.data_notificacao_3_dias} sobre o pet {v.pet.nome} com vacina vencendo em {v.data_proxima.strftime('%d/%m')}")
+                elif dias_para_vencimento == 3:
+                    print(f"Mensagem enviada para {numero} as {v.data_notificacao_3_dias} sobre o pet {v.pet.nome} com vacina vencendo em {v.data_proxima.strftime('%d/%m')}")
 
             except Exception as e:
                 print(f"Erro ao enviar para {numero} as {timezone.now()}: {e}")
